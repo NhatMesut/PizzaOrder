b4e5dd8bc1a72ad7ca722f63e026c83c33997db9
Pham Nhat Phuc <nhatphucpham@hotmail.com>
2024-07-16 07:44:46 +0700

    README

--- src/main/java/com/pizza/pizzaorder/PizzaOrderApplication.java
+++ src/main/java/com/pizza/pizzaorder/PizzaOrderApplication.java
@@ -1,6 +1,5 @@
 package com.pizza.pizzaorder;
 
-import com.pizza.pizzaorder.utils.SslUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PizzaOrderApplication {
 
     public static void main(String[] args) {
-        SslUtil.disableSSLVerification();
         SpringApplication.run(PizzaOrderApplication.class, args);
     }
 
